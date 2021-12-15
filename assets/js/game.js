6d78e835f7fe6ad53f794bccb6c575ca71b8de84
kdingman <kcdonlan@yahoo.com>
2021-12-14 19:02:10 -0500

    playername debugged

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,8 +154,20 @@ var shop = function() {
 /* End Game Funtions */
 
 /* Game Information / Variables */
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What's your robot's name?");
+  }
+
+  console.log("Your robot's name is" + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
